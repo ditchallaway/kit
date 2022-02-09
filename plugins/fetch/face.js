8f5fe66ac3854c39da95b70a0b94468670e6348d
jonnybot <in.the.air.jkb@gmail.com>
2022-02-08 18:08:42 -0800

    icons contact width javascript refactor

---
+++ plugins/fetch/face.js
@@ -0,0 +1,22 @@
+fetch(
+    "https://opensheet.elk.sh/1vkEHHOS4LG7K8jQpYBxA2sJJYfuP07nuku0tvdh-B10/4"
+  )
+    .then((res) => res.json())
+  
+    .then((data) => {
+        let output = ' ';
+      data.forEach((row) => {
+  
+          output += `
+          <tr class="border-bottom border-highlight">
+          <td class="p-1 font-700 color-theme">${row.serviceName}</td>
+          <td class="p-1 color-highlight">${row.serviceNote}</td>
+          <td class="p-1 color-theme">${row.servicePrice}</td>
+          </tr>
+          `;
+      });
+  
+          document.getElementById('face').innerHTML = output;
+   
+    
+  });
